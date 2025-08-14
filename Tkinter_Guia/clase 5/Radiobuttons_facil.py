62f6b31f9b73e789e5319e9e122ff9d201033d7a
byrondev <byron7891833@gmail.com>
2025-08-14 20:51:46 -0300

    hola

--- Tkinter_Guia/clase 5/Radiobuttons_facil.py
+++ Tkinter_Guia/clase 5/Radiobuttons_facil.py
@@ -1,8 +1,23 @@
 import tkinter as tk
 
 ventana = tk.Tk()
+variabledecontrol = tk.IntVar()
 
-opciones1 = tk.Radiobutton(ventanan, text="opcion 1", value=1
+def opcion_seleccionada():
+    color_opcion = variabledecontrol.get()  # Obtener el valor de la variable de control
+    if color_opcion == 0:
+        ventana.configure(bg="red")  
+    elif color_opcion == 1:
+        ventana.configure(bg="blue") 
+
+opciones1 = tk.Radiobutton(ventana, text="opcion 1", value=1, command=opcion_seleccionada)
+opciones2 = tk.Radiobutton(ventana, text="opcion 2", value=2, command=opcion_seleccionada)
+
+opciones1.pack()
+opciones2.pack()
+
+
+
 
 
 ventana.mainloop()
