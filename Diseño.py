5c8683b0e22b532309a96b8c6c2d2afe3892ed3b
Doctordog <thiagodiaz2574@gmail.com>
2025-08-25 23:30:40 -0300

    xd

--- Diseño.py
+++ Diseño.py
@@ -1,27 +1,53 @@
 import tkinter as tk
+from PIL import Image, ImageTk
 
 ventana = tk.Tk()
 ventana.title("Fravega")
 ventana.geometry("800x100")
 
-
-
 def crear_header(ventana):
-    #la parte de arriba
-    header = tk.Frame(ventana, bg="DarkOrchid1", height=60)
+    header = tk.Frame(ventana, bg="DarkOrchid1", height=70)
     header.pack(fill="x")
 
-    #logito
-    logo = tk.Label(header, text="FRÁVEGA", font=("Arial", 16, "bold"), bg="lightgray", fg="purple")
+    
+    fravega_no = Image.open("Imagenes/logo-fravega.png").convert("RGBA")
+    
+    
+    alto_header = 70
+    ancho_logo = int(fravega_no.width * (alto_header / fravega_no.height))
+    fravega_no = fravega_no.resize((ancho_logo, alto_header), Image.LANCZOS)
+
+    fravega_logo = ImageTk.PhotoImage(fravega_no)
+    logo = tk.Label(header, image=fravega_logo, bg="DarkOrchid1")
+    logo.image = fravega_logo
     logo.pack(side="left", padx=10)
 
-    # bienvenida
-    ubicacion = tk.Label(header, text="Bienvenido niko",font=("Arial"))
+   
+    ubicacion = tk.Label(header, text="Bienvenido niko!", font=("Impacto",18),fg="white", bg="DarkOrchid1")
     ubicacion.pack(side="left", padx=10)
 
 
+
+    usuario_no = Image.open("Imagenes/Usuario.png").convert("RGBA")
+
+
+    alto_header = 50  
+    ancho_usuario = int(usuario_no.width * (alto_header / usuario_no.height))
+
+
+    usuario_red = usuario_no.resize((ancho_usuario, alto_header), Image.LANCZOS)
+
+    # Convertir a PhotoImage para Tkinter
+    usuario = ImageTk.PhotoImage(usuario_red)
+
+  
+    # Crear botón con la imagen
+    boton_usuario = tk.Button(header, image=usuario,bg="DarkOrchid1",activebackground="white",bd=0)
+    boton_usuario.image = usuario
+    boton_usuario.pack(side="right", padx=10)
+
+
+
+
 crear_header(ventana)
-
-
-
 ventana.mainloop()
